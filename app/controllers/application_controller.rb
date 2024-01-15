5a77e1a4111230313b4a0b21286a3f3da7fb2a45
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-15 18:14:55 +0900

    username・password環境変数設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+# PAYJPで使用する環境変数
+# PAYJP_PUBLIC_KEY = your_payjp_public_key
+# PAYJP_SECRET_KEY = your_payjp_secret_key
 end
