cd3f387abea6e90a1a95901d5b9611db1a37576c
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-18 16:19:32 +0900

    パスワードのバリデーション（半角英数字）

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password, presence: true
+  validates :encrypted_password, presence: true, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
