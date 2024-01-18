815d32aaf3adbf7f404c3c53767c3979bfe52567
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-18 18:22:50 +0900

    新規登録時のバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password, presence: true, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, presence: true, format: {with: /\A[a-z0-9]+\z/i}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
   validates :birthday, presence: true
 end
