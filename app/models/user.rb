c7929ffb9b0aa7d93911987d93338d3e751e385d
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-19 18:19:47 +0900

    重複バリデーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items, dependent: :destroy
-  has_many :orders, dependent: :destroy
+  # has_many :items, dependent: :destroy
+  # has_many :orders, dependent: :destroy
 
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, format: {with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i }
+  validates :password, format: {with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i }
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
