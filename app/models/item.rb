64c0840fc99139d3545fdce51494bb8b9b8fb1f9
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-22 16:23:27 +0900

    （image）バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :days_to_ship
   has_one_attached :image
 
-  validates :name, :description, :user_id, presence: true
+  validates :name, :description, :user_id, :image, presence: true
   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :category_id, :condition_id, :shipping_charge_id, :state_province_id, :days_to_ship_id, numericality: { other_than: 1 }
 end
