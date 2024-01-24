e01eefd0061572df6239b56728a445b128a5a0a7
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-24 15:20:22 +0900

    showルーティング・V・Cの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -16,6 +17,14 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+  
+  def show
+  end
+  
+  
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
