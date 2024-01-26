d6f86d0d1098fae17491ce57bcd9516362bafc3d
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-26 16:38:23 +0900

    addressモデル生成・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :state_province
   belongs_to :days_to_ship
   has_one_attached :image
+  has_one :order
 
   validates :name, :description,:image, presence: true
   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
